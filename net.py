9765c9eb009833be369b730bfd5f1e5d59d1c11d
dcr <1456882404@qq.com>
2022-09-29 23:50:28 +0800

    finish lab5

--- net.py
+++ net.py
@@ -31,13 +31,18 @@ class Net:
         self._network = np.zeros((self._point_number, self._point_number), dtype=int)
         self._min_dis = np.full((self._point_number, self._point_number), -1, dtype=int)
         self._min_route = [None] * self._point_number
+
+        # 构建对应的路线矩阵
         for i in range(0, self._point_number):
             self._min_route[i] = [None] * self._point_number
             for j in range(0, self._point_number):
                 self._min_route[i][j] = [i]
+                self._min_route[i][j].append(j)
 
+        # 构建对应的距离矩阵
         for item in self._net:
             self._network[item[0], item[1]] = item[2]
+            self._network[item[1], item[0]] = item[2]
 
         print('=======================================================================')
         print('Q1:  The size of the points is :', end='\n')
@@ -77,7 +82,14 @@ class Net:
         plt.scatter(index, list)
         plt.show()
 
+    def ResetRoute(self, i, j, k):
+        self._min_dis[i][j] = self._min_dis[i][k] + self._min_dis[k][j]
+        self._min_route[i][j] = self._min_route[i][k] + self._min_route[k][j]
+        self._min_route[i][j].remove(k)
+
     def FindMinRoute(self):
+        print('Begin to find the min route')
+        # 读入对应的测试数据
         f = open('test_data.txt', encoding='UTF-8')
         test_data = np.zeros((4, 2), dtype=int)
         line = f.readline()
@@ -89,5 +101,20 @@ class Net:
             i += 1
             line = f.readline()
 
-        f.close()
+        print('test')
+        # 下面使用弗洛伊德算法计算最短路径
+        for i in range(0, self._point_number):
+            print(i)
+            for j in range(0, self._point_number):
+                for k in range(0, self._point_number):
+                    if self._min_dis[i][k] > 0 & self._min_dis[k][j] > 0:
+                        if self._min_dis[i][j] > 0:
+                            if self._min_dis[i][k] + self._min_dis[k][j] < self._min_dis[i][j]:
+                                self.ResetRoute(i, j, k)
+                        else:
+                            self.ResetRoute(i, j, k)
 
+        print('Finish floyd')
+        for item in test_data:
+            print(self._min_route[item[0]][item[j]])
+        f.close()
